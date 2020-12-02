8cbce72c627c2e957e1c3b30249815c2083eba6f
lampo4ka-j <sasha.klimkin.002@mail.ru>
2020-12-02 12:26:17 +0300

    first commit 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () =>{
+    return (
+    <header className='header'>
+    <img src="https://im0-tub-ru.yandex.net/i?id=fea656fc237b520cebd5f898f005feff&n=13&exp=1"></img>
+  </header>
+    )
+}
+
+export default Header;
